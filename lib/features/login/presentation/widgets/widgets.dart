5e56e679863101d63b8ff72431b86222ef3fdee1
Federico Perez Morales <federicoperez61@gmail.com>
2023-02-22 14:20:19 -0500

    fix: Login feature renamed to Authentication

--- lib/features/login/presentation/widgets/widgets.dart
+++
@@ -1,4 +0,0 @@
-export 'package:real_time_chat/features/login/presentation/widgets/labels.dart';
-export 'package:real_time_chat/features/login/presentation/widgets/custom_filled_form_button.dart';
-export 'package:real_time_chat/features/login/presentation/widgets/custom_text_field.dart';
-export 'package:real_time_chat/features/login/presentation/widgets/logo.dart';
