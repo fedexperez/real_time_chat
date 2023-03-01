27e8f2221a1579f4ebff04e7a297bdef62e6e754
Federico Perez Morales <federicoperez61@gmail.com>
2023-03-01 10:08:48 -0500

    chore: Token Exception and Failure
     -  Exceptions
     -  Failures

--- lib/core/errors/exceptions.dart
+++ lib/core/errors/exceptions.dart
@@ -1,3 +1,5 @@
 class ServerException implements Exception {}
 
 class ConnectionException implements Exception {}
+
+class TokenException implements Exception {}
--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -13,3 +13,5 @@ abstract class Failure extends Equatable {
 class ServerFailure extends Failure {}
 
 class ConnectionFailure extends Failure {}
+
+class TokenFailure extends Failure {}
